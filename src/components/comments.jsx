1da224818d40be78d15aa474295eae8c36f12c59
Mrcode93 <alhlaqalhlaq667@gmail.com>
2024-03-11 01:32:41 +0300

    v.5.1

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,24 +1,24 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-const Comments = () => {
-  const [comments, setComments] = useState([]);
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// const Comments = () => {
+//   const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:8080/comment").then((res) => {
-      setComments(res.data);
-      console.log(res.data);
-    });
-  }, []);
-  return (
-    <div className="comments">
-      {comments.map((comment) => {
-        <div className="comment">
-          <p>{comment.comment}</p>
-          <p>{comment.user}</p>
-        </div>;
-      })}
-    </div>
-  );
-};
+//   useEffect(() => {
+//     axios.get("http://localhost:8080/comment").then((res) => {
+//       setComments(res.data);
+//       console.log(res.data);
+//     });
+//   }, []);
+//   return (
+//     <div className="comments">
+//       {comments.map((comment) => {
+//         <div className="comment">
+//           <p>{comment.comment}</p>
+//           <p>{comment.user}</p>
+//         </div>;
+//       })}
+//     </div>
+//   );
+// };
 
-export default Comments;
+// export default Comments;
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,6 +1,55 @@
-// projects.jsx
+// // projects.jsx
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// const Projects = () => {
+//   const [projectsList, setProjectsList] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:8080/project")
+//       .then((res) => {
+//         setProjectsList(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   });
+
+//   return (
+//     <div className="projects">
+//       <div className="main">
+//         <h1> Projects </h1>{" "}
+//         <div className="projects-list">
+//           {" "}
+//           {projectsList.map((project) => (
+//             <div key={project._id} className="project-item">
+//               <h4> {project.title} </h4> <img src={project.image} alt="image" />
+//               <p> {project.description} </p>{" "}
+//               <p className="link"> {project.link} </p>{" "}
+//               <p className="demo"> {project.demo} </p>
+//               <span className="pixel-1"> </span>{" "}
+//               <span className="pixel-2"> </span>{" "}
+//               <span className="pixel-3"> </span>{" "}
+//               <span className="pixel-4"> </span>{" "}
+//               <span className="pixel-5"> </span>{" "}
+//               <span className="pixel-6"> </span>{" "}
+//               <span className="pixel-7"> </span>{" "}
+//               <span className="pixel-8"> </span>{" "}
+//               <span className="pixel-9"> </span>{" "}
+//               <span className="pixel-10"> </span>{" "}
+//             </div>
+//           ))}{" "}
+//         </div>{" "}
+//       </div>{" "}
+//     </div>
+//   );
+// };
+
+// export default Projects;
+
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 const Projects = () => {
   const [projectsList, setProjectsList] = useState([]);
 
@@ -9,11 +58,12 @@ const Projects = () => {
       .get("http://localhost:8080/project")
       .then((res) => {
         setProjectsList(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []); // Pass an empty dependency array to run the effect only once
 
   return (
     <div className="projects">
@@ -23,20 +73,24 @@ const Projects = () => {
           {" "}
           {projectsList.map((project) => (
             <div key={project._id} className="project-item">
-              <h4> {project.title} </h4> <img src={project.image} alt="image" />
+              <h4> {project.title} </h4>{" "}
+              {/* Assuming the 'image' field contains the filename */}{" "}
+              <a href={project.demo} target="_blank">
+                <img
+                  src={`http://localhost:8080/uploads/${project.image}`}
+                  alt="Project Image"
+                />
+              </a>
               <p> {project.description} </p>{" "}
-              <p className="link"> {project.link} </p>{" "}
-              <p className="demo"> {project.demo} </p>
-              <span className="pixel-1"> </span>{" "}
-              <span className="pixel-2"> </span>{" "}
-              <span className="pixel-3"> </span>{" "}
-              <span className="pixel-4"> </span>{" "}
-              <span className="pixel-5"> </span>{" "}
-              <span className="pixel-6"> </span>{" "}
-              <span className="pixel-7"> </span>{" "}
-              <span className="pixel-8"> </span>{" "}
-              <span className="pixel-9"> </span>{" "}
-              <span className="pixel-10"> </span>{" "}
+              <a className="link" href={project.link}>
+                Code Link
+              </a>{" "}
+              {/* The pixel spans - adjust as needed */}{" "}
+              {[...Array(10)].map((_, index) => (
+                <span key={index} className={`pixel-${index + 1}`}>
+                  {" "}
+                </span>
+              ))}{" "}
             </div>
           ))}{" "}
         </div>{" "}
